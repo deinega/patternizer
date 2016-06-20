43786a4e9022378afda6e93c25714c22af46c025
grishan <poblizosti@mail.ru>
2016-06-20 00:57:07 -0400

    distr

--- include/series.h
+++ include/series.h
@@ -3,10 +3,14 @@
 
 #include "quandl.h"
 
-
-
 double calculateAverage(vector<double> &vals);
 
 double calculateAutocorrelation(vector<double> &vals, int shift, double *av);
+
+vector<double> generateMarkovProces(int sz, const double start, const double mu, const double sigma);
+
+double normal(double mu, double sigma);
+
+vector<double> calculateDistribution(vector<double> in, double min, double max, int sz);
 
 #endif
