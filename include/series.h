174235d73d9e2cf5637a829f0fde6947e1bec0ac
grishan <poblizosti@mail.ru>
2016-06-21 22:17:03 -0400

    it

--- include/series.h
+++ include/series.h
@@ -3,9 +3,40 @@
 
 #include "quandl.h"
 
-double calculateAverage(vector<double> &vals);
+template<class it_t>
+double calculateAverage(it_t it, int sz){
+    double av = 0;
+    for(int i=0; i<sz; i++, it++){
+        av += *it;
+    }
+    av /= sz;
+    return av;
+}
 
-double calculateAutocorrelation(vector<double> &vals, int shift, double *av);
+template<class it_t>
+double calculateAutocorrelation(it_t it, int sz, int shift, double *av_calculated){
+    double av = av_calculated ? *av_calculated : calculateAverage(it, sz);
+    
+    double aut = 0;
+    
+    for(int i = 0; i<sz-shift; i++, it++){
+        aut += (*(it + shift) - av) * (*it - av);
+    }
+    aut /= sz - 1 - shift;
+    
+    return aut;
+}
+
+template<class it_t>
+double calculateCorrelation(it_t it1, it_t it2, int sz){
+    double av1 = calculateAverage(it1, sz);
+    double av2 = calculateAverage(it2, sz);
+    double corr = 0;
+    for(int i=0; i<sz; i++, it1++, it2++){
+        corr += (*it1 - av1) * (*it2 - av2);
+    }
+    return corr;
+}
 
 vector<double> generateMarkovProces(int sz, const double start, const double mu, const double sigma);
 
